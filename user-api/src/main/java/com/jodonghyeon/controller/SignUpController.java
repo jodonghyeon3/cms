4840aae99bbede8139a336cc68d1564c69689c9a
jodonghyeon <jawoo1003@gmail.com>
2023-07-14 01:59:02 +0900

    feat : [002-SIGN-UP] 커스터머(고객)에 대한 Signup Service 구현

---
+++ user-api/src/main/java/com/jodonghyeon/controller/SignUpController.java
@@ -0,0 +1,28 @@
+package com.jodonghyeon.controller;
+
+
+import com.jodonghyeon.application.SignUpApplication;
+import com.jodonghyeon.domain.SignUpForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/signup")
+public class SignUpController {
+
+    private final SignUpApplication signUpApplication;
+
+    @PostMapping
+    public ResponseEntity<String> customerSignUp(@RequestBody SignUpForm form) {
+        return ResponseEntity.ok(signUpApplication.customerSingUp(form));
+    }
+
+    @PutMapping("/verify/customer")
+    public ResponseEntity<String> verifyCustomer(String email, String code) {
+        signUpApplication.customerVerify(email, code);
+        return ResponseEntity.ok("인증이 완료되었습니다.");
+    }
+
+}
---
+++ user-api/src/main/java/com/jodonghyeon/domain/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.jodonghyeon.domain.repository;
+
+
+import com.jodonghyeon.domain.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+}
+
