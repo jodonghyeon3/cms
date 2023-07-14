89d6a929c5801165307a491f710ade65bc2f20e0
jodonghyeon <jawoo1003@gmail.com>
2023-07-14 14:28:38 +0900

    feat : [003-EMAIL-VERIFICATION] 커스터머 이메일 verification.

---
+++ user-api/src/main/java/com/jodonghyeon/controller/SignInController.java
@@ -0,0 +1,10 @@
+package com.jodonghyeon.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/signIn")
+public class SignInController {
+
+}
---
+++ user-api/src/main/java/com/jodonghyeon/domain/SignInForm.java
@@ -0,0 +1,9 @@
+package com.jodonghyeon.domain;
+
+import lombok.Getter;
+
+@Getter
+public class SignInForm {
+    private String email;
+    private String password;
+}
